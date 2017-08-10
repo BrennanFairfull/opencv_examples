34d6b69431b6f127fc38fbb159effe6548094d83
Bren <brennan.fairfull@gmail.com>
2017-08-10 11:23:18 +1000

    added gloabl variable for subscriber topic in cv_bridger

--- src/cv_bridger.cpp
+++ src/cv_bridger.cpp
@@ -5,6 +5,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+static const std::string SUB_TOPIC = "";
 
 class ImageConverter
 {
@@ -18,7 +19,7 @@ public:
     : it_(nh_)
   {
     // Subscrive to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/camera/image_raw", 1,
+    image_sub_ = it_.subscribe(SUB_TOPIC, 1,
       &ImageConverter::imageCb, this);
     image_pub_ = it_.advertise("/image_converter/output_video", 1);
   }
